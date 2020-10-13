9ddd4696ad53056436573fda85609748a39c6c4a
Larry Caichihua Vergara <lcaichihua@hotmail.com>
2020-10-13 02:31:16 -0500

    first commit

---
+++ src/main/java/com/cavetech/spring/security/jwt/SpringJwtSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cavetech.spring.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSecurityExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cavetech/spring/security/jwt/SpringJwtSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavetech.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
